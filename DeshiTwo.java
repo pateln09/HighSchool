5bf938868efb5903f72b5720d1e17a1a300a0b82
Neel Patel <72580800+pateln09@users.noreply.github.com>
2023-08-21 22:44:24 -0500

    Add files via upload

--- DeshiTwo.java
+++ DeshiTwo.java
@@ -1,4 +1,15 @@
+import java.io.*;
+import java.util.*;
 
 public class DeshiTwo {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scan = new Scanner (new File ("Deshi.dat"));
+		
+		
+			while (scan.hasNext()) {
+				String str = scan.nextLine();
+				System.out.println((str.matches("(.*[aeiou]){1,4}(.*[^aeiou]){1,7}"))? "ACCEPTABLE": "NOT ACCEPTABLE");
+			}
+	}
 
 }
